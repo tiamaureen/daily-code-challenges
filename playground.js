1c0649ebf0513ca587fbe34e9970d7c4227b205e
tiamaureen <github@tia.maureen.email>
2022-10-14 17:20:39 -0400

    complete coding challenge two

--- playground.js
+++ playground.js
@@ -1,4 +1,29 @@
+//*Coding Challenge #2
 
+// Grasshopper - Personalized Message by danleavitt0 on CodeWars
+
+// Description:
+
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+// Use conditionals to return the proper message:
+// case 	              return
+// name equals owner 	'Hello boss'
+// otherwise 	        'Hello guest'
+
+// function greet (name, owner) {
+//   if (name === owner) {
+//     return 'Hello boss'
+//   } else {
+//     return 'Hello guest'
+//   }
+// }
+
+// function greet (name, owner) {
+//   return name === owner ? 'Hello boss' : 'Hello guest'
+// }
+
+//*Coding Challenge #1
 
 // The Feast of Many Beasts By MollyJeanB-pdx on CodeWars
 
@@ -8,10 +33,10 @@
 
 // Assume that beast and dish are always lowercase strings, and that each has at least two letters. beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string. They will not contain numerals.
 
-function feast(beast, dish) {
-  return beast[0] === dish[0] && beast[beast.length-1] === dish[dish.length-1]
-}
+// function feast(beast, dish) {
+//   return beast[0] === dish[0] && beast[beast.length-1] === dish[dish.length-1]
+// }
 
-function feast(beast, dish) {
-  return beast.startsWith(dish[0]) && beast.endsWith(dish[dish.length-1])
-}
+// function feast(beast, dish) {
+//   return beast.startsWith(dish[0]) && beast.endsWith(dish[dish.length-1])
+// }
